cd92076447479df84b04f6eb4166594b4e728a2c
Johnnie2016 <j.morrison12@rgu.ac.uk>
2016-08-18 12:39:00 +0100

    Resit attempt 18th Aug

---
+++ diary.sql
@@ -0,0 +1,5 @@
+diaryEntryID,entryTitle,entrySummary,category
+1,Today at work,Today I went to work and did lots of complicated things. I wrote a program in php and then connected it to a MySQL database,Work
+11,University Lecturer,"This week in university I had an amazing lecture. I can't remember the name of the lecturer but he was really, really good",University
+21,Dads new television,Today Dad bought a new television. He likes to watch the F1 on it a lot.,Family
+
