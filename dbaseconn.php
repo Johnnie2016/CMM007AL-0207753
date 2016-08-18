e7906f0c9147106adf533783f30e5b89a35d401f
Johnnie2016 <j.morrison12@rgu.ac.uk>
2016-08-18 12:37:08 +0100

    Resit attempt 18th Aug

---
+++ dbaseconn.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_SERVER', 'eu-cdbr-azure-north-e.cloudapp.net');
+define('DB_USERNAME', 'b44975614ac842');
+define('DB_PASSWORD', 'faa37d47');
+define('DB_DATABASE', 'CMM007ALDB-0207753');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
